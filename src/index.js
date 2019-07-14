4db67e46dec9df125b3d30bbeee9ce6358ab230b
Phillip Darby <darby.phillip@gmail.com>
2019-07-14 19:28:30 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const debug = require('debug')('server:debug');
+import config from 'config';
+import express from 'express';
+
+const app = express();
+
+const listen = app.listen(config.get('port'), () => {
+  debug(
+    `Server is running on port ${config.get('port')} and in ${config.get(
+      'name'
+    )} mode`
+  );
+  console.log(
+    `Server is running on port ${config.get('port')} and in ${config.get(
+      'name'
+    )} mode`
+  );
+});
+
+module.exports = app;
+module.exports.port = listen.address().port;
